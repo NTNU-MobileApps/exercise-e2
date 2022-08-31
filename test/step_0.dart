8018e52bda61bb8629e5f6eea088526bed55a18f
Girts <girts.strazdins@gmail.com>
2022-08-31 10:54:35 +0200

    Experiment - update tests for the main workflow, to avoid failing

---
+++ test/step_0.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+/// This is a dummy test for the GitHub workflow on each commit to the master branch
+void main() {
+  test('Step 1 - Animal class', () {
+    expect(6 * 7, 42);
+  });
+}
